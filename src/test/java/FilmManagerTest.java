2acf9cb1a23a2d4ee75a9504bbf28b51fb84da7b
nrr <nrr@warsong.me>
2022-06-03 20:29:03 +0300

    still having fun with JaCoCo

--- src/test/java/FilmManagerTest.java
+++ src/test/java/FilmManagerTest.java
@@ -3,23 +3,23 @@ import org.junit.jupiter.api.Test;
 import ru.netology.*;
 
 public class FilmManagerTest {
-    Film first =  new Film(1,"one","one");
-    Film second =  new Film(2,"two","two");
-    Film third =  new Film(3,"three","three");
-    Film fourth =  new Film(4,"four","four");
-    Film fifth =  new Film(5,"five","five");
-    Film sixth =  new Film(6,"six","six");
-    Film seventh =  new Film(7,"seven","seven");
-    Film eighth =  new Film(8,"eight","eight");
-    Film ninth =  new Film(9,"nine","nine");
-    Film tenth =  new Film(10,"ten","ten");
-    Film eleventh =  new Film(11,"eleven","eleven");
-    Film twelfth =  new Film(12,"twelve","twelve");
-    Film thirteenth =  new Film(13,"thirteen","thirteen");
-    Film fourteenth =  new Film(14,"fourteen","fourteen");
-    Film fifteenth =  new Film(15,"fifteen","fifteen");
-    Film sixteenth =  new Film(16,"sixteen","sixteen");
-    Film seventeenth =  new Film(17,"seventeen","seventeen");
+    Film first = new Film(1, "one", "one");
+    Film second = new Film(2, "two", "two");
+    Film third = new Film(3, "three", "three");
+    Film fourth = new Film(4, "four", "four");
+    Film fifth = new Film(5, "five", "five");
+    Film sixth = new Film(6, "six", "six");
+    Film seventh = new Film(7, "seven", "seven");
+    Film eighth = new Film(8, "eight", "eight");
+    Film ninth = new Film(9, "nine", "nine");
+    Film tenth = new Film(10, "ten", "ten");
+    Film eleventh = new Film(11, "eleven", "eleven");
+    Film twelfth = new Film(12, "twelve", "twelve");
+    Film thirteenth = new Film(13, "thirteen", "thirteen");
+    Film fourteenth = new Film(14, "fourteen", "fourteen");
+    Film fifteenth = new Film(15, "fifteen", "fifteen");
+    Film sixteenth = new Film(16, "sixteen", "sixteen");
+    Film seventeenth = new Film(17, "seventeen", "seventeen");
 
     @Test
     void shouldAddFilmToEmptyManager() {
@@ -28,5 +28,36 @@ public class FilmManagerTest {
 
         Film[] er = {first};
         Assertions.assertArrayEquals(er, fm.getFilms());
+    }
+
+    @Test
+    void shouldAddFilmToNotEmptyManager() {
+        FilmManager fm = new FilmManager();
+        fm.addFilm(first);
+        fm.addFilm(second);
+
+        Film[] expectedResult = {first, second};
+        Assertions.assertArrayEquals(expectedResult, fm.getFilms());
+    }
+
+    @Test
+    void shouldFindBlankResultWithDefConstr() {
+        FilmManager flm = new FilmManager();
+        Film[] expected = {};
+        Assertions.assertArrayEquals(expected, flm.findAll());
+    }@Test
+    void shouldFindBlankResultWithCustomConstr() {
+        FilmManager flm = new FilmManager(5);
+        Film[] expected = {};
+        Assertions.assertArrayEquals(expected, flm.findAll());
+    }
+
+    @Test
+    void shouldFindLessThenLimitWithDefConstr() {
+        FilmManager fm = new FilmManager();
+        fm.addFilm(first);
+
+        Film[] er = {first};
+        Assertions.assertArrayEquals(er, fm.findAll());
     }
 }
